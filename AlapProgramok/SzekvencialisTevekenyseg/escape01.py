2d7476a2b5a32b37642a9a334c879d35634025a3
resahhdev <resahhdevelop@gmail.com>
2020-07-28 12:40:30 +0200

    Könyvtárszerkezet

---
+++ AlapProgramok/SzekvencialisTevekenyseg/escape01.py
@@ -0,0 +1,12 @@
+"""
+Írassa a képernyőre a következőket:
+
+1    alma    500
+2    szilva  700
+3    barack  850
+4    körte   370
+Az egyes oszlopok között tabulátor legyen.
+Minden egyes sort csak egy kiírató utasítással írasson ki.
+Használjon escape szekvenciát a tabulátorokhoz és a sortöréshez.
+"""
+print( "1\talma\t500\n2\tszilva\t700\n3\tbarack\t850\n4\tkörte\t370" )
